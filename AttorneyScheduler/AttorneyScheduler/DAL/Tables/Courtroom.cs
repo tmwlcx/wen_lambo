5727522256829b5a5c1e5e33f4667d1d6ab0d674
tmwlcx <tim.wilcox@gatech.edu>
2025-01-27 19:37:17 -0700

    attemtpting to fix the issue with the wrong type for courtRoomNumber (string)...lets see...

--- AttorneyScheduler/AttorneyScheduler/DAL/Tables/Courtroom.cs
+++ AttorneyScheduler/AttorneyScheduler/DAL/Tables/Courtroom.cs
@@ -3,7 +3,7 @@ namespace AttorneyScheduler.DAL.Tables
     public class Courtroom
     {
         public int CourtRoomId { get; set; }
-        public int CourtRoomNumber { get; set; }
+        public string CourtRoomNumber { get; set; }
         public DateTime CreatedDate { get; set; }
         public DateTime UpdatedDate { get; set; }
     }
